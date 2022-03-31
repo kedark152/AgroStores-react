82d1ae58b864ea51084be8da0d7399c8f34f6643
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-03-31 18:07:42 +0530

    Update allComponents.js

--- src/components/allComponents.js
+++ src/components/allComponents.js
@@ -3,6 +3,6 @@ import { FiltersBar } from "./FiltersBar"
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer"
 import { ProductCard } from "./ProductCard";
-import {HorizontalCard} from "./HorizontalCard";
 
-export {FeaturedCategories,FiltersBar,Navbar,Footer,ProductCard,HorizontalCard};
+
+export {FeaturedCategories,FiltersBar,Navbar,Footer,ProductCard};
