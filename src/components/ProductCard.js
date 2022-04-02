7ab04df15036584607d455ce11c66f81541111e6
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-04-02 14:42:02 +0530

    feat: added actions to product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,19 @@
-
+import { useCart } from "../context/cart-context";
+import { useWishList } from "../context/wishlist-context";
+import { Link } from "react-router-dom";
+import { IsItemInCart } from "../utils/isItemInCart";
+import { IsItemInWishList } from "../utils/isItemInWishList";
 // eslint-disable-next-line react/prop-types
 export const ProductCard =({cardDetails}) =>{
   // eslint-disable-next-line react/prop-types
-  const {title, imgUrl,price,price_old,discount, rating,  isBestSeller,  isOutOfStock} = cardDetails;
+  const { _id, title, imgUrl,price,price_old,discount, rating,  isBestSeller,  isOutOfStock} = cardDetails;
+  const {dispatchCart} =useCart();
+  const {dispatchWishList} = useWishList();
+
+  const inWishList = IsItemInWishList(_id);
+  const inCart = IsItemInCart(_id);
+
+
     return (
             <>
              {/* Card  */}
@@ -11,7 +22,12 @@ export const ProductCard =({cardDetails}) =>{
             { isOutOfStock && <div className="card-overlay-container">
                 <div className="card-overlay-text">Out of Stock</div>
                </div> }
-          <i className="material-icons heart-icon">favorite_border</i>
+          
+               
+          {inWishList?<i className="material-icons heart-icon-product-list" onClick={()=>dispatchWishList({type:"REMOVE-FROM-WISHLIST",payload:cardDetails})}>favorite</i>
+                     :<i className="material-icons heart-icon-product-list" onClick={()=>dispatchWishList({type:"ADD-TO-WISHLIST",payload:cardDetails})}>favorite_border</i>
+          }
+          
           <div className="card-img-box">
             <a href="#">
               <img
@@ -32,10 +48,17 @@ export const ProductCard =({cardDetails}) =>{
             <p className="card-price-cut">₹{price_old}</p>
             <p className="card-percent-discount">{discount}% off</p>
           </div>
-          <a href="#" className="btn btn-solid-icon fw-bold">
-            <i className="material-icons">shopping_cart</i>
-            Add to Cart
-          </a>
+
+              {inCart?
+              (<Link to="/cart" className="btn btn-outline-icon fw-bold">
+                  <i className="material-icons">shopping_cart</i>
+                  Go to Cart
+                </Link>)
+                :(<a href="#" className="btn btn-solid-icon fw-bold" onClick={()=>dispatchCart({type:"ADD-TO-CART",payload: { _id,title, imgUrl,price,price_old,discount,quantity:1}})}>
+                <i className="material-icons">shopping_cart</i>
+                Add to Cart
+              </a>)}
+        
         </div>
     </>
     )
