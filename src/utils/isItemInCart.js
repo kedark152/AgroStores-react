4cc5fca57b0a52c5ce6242d2f7029cc4b6b9bab9
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-07-13 18:30:03 +0530

    Update isItemInCart.js

--- src/utils/isItemInCart.js
+++ src/utils/isItemInCart.js
@@ -1,11 +1,12 @@
-import {useCart} from "../context/cart-context";
+import { useCart } from "../context/cart-context";
 
-export const IsItemInCart = (id)=>{
-  const {cartState} = useCart();
- let itemStatus = false;
-  cartState.cartItems.map((item)=>{
-    item._id === id ? (itemStatus = true) : null
-}
-  )
+export const IsItemInCart = (id) => {
+  const { cartState } = useCart();
+  let itemStatus = false;
+  if (cartState.cartItems.length > 0) {
+    cartState.cartItems.map((item) => {
+      item._id === id ? (itemStatus = true) : null;
+    });
+  }
   return itemStatus;
-}
+};
