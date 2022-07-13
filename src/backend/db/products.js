82ce5db5fbb8d5d0e73252bbc21c246fc98cf510
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-07-13 18:11:24 +0530

    feat: updated backend db with product description

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,5 +1,3 @@
-import { v4 as uuid } from "uuid";
-
 /**
  * Product Database can be added here.
  * You can add products of your wish with different attributes
@@ -7,147 +5,285 @@ import { v4 as uuid } from "uuid";
 
 export const products = [
   {
-    _id: uuid(),
+    _id: "I5c1bFwd",
     title: "TrustBasket Organic Vermicompost Fertilizer Manure for Plants(5KG)",
-    imgUrl:"https://m.media-amazon.com/images/I/71iPA5LfB7L._AC_UL320_.jpg",
+    imgUrl: "https://m.media-amazon.com/images/I/71iPA5LfB7L._AC_UL320_.jpg",
     price: 332,
-    price_old:349,
-    discount:5, 
+    price_old: 349,
+    discount: 5,
     rating: 4.5,
     isBestSeller: true,
     isOutOfStock: false,
     categoryName: "Fertilizers",
+    productDetails: {
+      Brand: "TrustBasket",
+      Weight: "5000 Grams",
+      Form: "Powder",
+      Volume: "5 Litres",
+    },
+    description: `1. 100% organic
+            2. Improves soil aeration
+            3. Enriches soil with micro-organisms (adding enzymes such as phosphate and cellulose)
+            4. Microbial activity in worm castings is 10 to 20 times higher than in the soil and organic matter that the worm ingests
+            5. Attracts deep-burrowing earthworms already present in the soil.
+            6. Increases the immune system of plants.
+            7. Easy to store and easy to use.`,
   },
   {
-    _id: uuid(),
-    title: "TrustBasket Enriched Organic Earth Magic Potting Soil Fertilizer for Plants(5KG)",
-    imgUrl:"https://m.media-amazon.com/images/I/61fUoGkNdHL._AC_UL320_.jpg",
+    _id: "VhibWhCO",
+    title:
+      "TrustBasket Enriched Organic Earth Magic Potting Soil Fertilizer for Plants(5KG)",
+    imgUrl: "https://m.media-amazon.com/images/I/61fUoGkNdHL._AC_UL320_.jpg",
     price: 950,
-    price_old:1999,
-    discount:52, 
+    price_old: 1999,
+    discount: 52,
     rating: 3.3,
     isBestSeller: true,
     isOutOfStock: false,
     categoryName: "Fertilizers",
+    productDetails: {
+      Brand: "TrustBasket",
+      Weight: "5000 Grams",
+      Form: "Granules",
+      Volume: "5 Litres",
+    },
+    description: `Contains microbes which enhance the soil properties
+              Completely organic and does not contain any harmful chemicals
+              Contains micro and macro nutrients. Has good water holding capacity
+              Its antifungal property helps the plants to grow healthy`,
   },
   {
-    _id: uuid(),
-    title: "Fieldstar 8-Litre Plastic Manual Sprayer for spraying fertilizers in Farms.",
-    imgUrl:"https://m.media-amazon.com/images/I/61xnYxYWFsL._AC_UL320_.jpg",
+    _id: "w1zV3ayR",
+    title:
+      "Fieldstar 8-Litre Plastic Manual Sprayer for spraying fertilizers in Farms.",
+    imgUrl: "https://m.media-amazon.com/images/I/61xnYxYWFsL._AC_UL320_.jpg",
     price: 950,
-    price_old:1999,
-    discount:52, 
+    price_old: 1999,
+    discount: 52,
     rating: 4.8,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Farming Tools",
+    productDetails: {
+      Brand: "FIELDSTAR",
+      Material: "Others",
+      Color: "Green",
+    },
+    description: `Used for spraying water and pesticides
+          Package Contents: 1 Sprayer
+          8 LITRE TANK
+          BEST QUALITY`,
   },
   {
-    _id: uuid(),
-    title: "FreshDcart Solar Soil Plant Care New 3 In 1 Water Moisture Soil Sensor.",
-    imgUrl:"https://m.media-amazon.com/images/I/71WLLgqaUCL._AC_UL320_.jpg",
+    _id: "sP6NCOgs",
+    title:
+      "FreshDcart Solar Soil Plant Care New 3 In 1 Water Moisture Soil Sensor.",
+    imgUrl: "https://m.media-amazon.com/images/I/71WLLgqaUCL._AC_UL320_.jpg",
     price: 589,
-    price_old:1010,
-    discount:63, 
+    price_old: 1010,
+    discount: 63,
     rating: 3.8,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Electronic Devices",
+    productDetails: {
+      Brand: "FreshDcart",
+      Material: "Others",
+      DeviceType: "Electronic",
+    },
+    description: `Plant Care New 3 in 1 Hydroponic Plants Soil Moisture PH Light Meter good for agriculture soil testing use
+    Scientifically Accurate: Easy to read water moisture, ph and light levels, promotes healthy plants`,
   },
   {
-    _id: uuid(),
+    _id: "wxOaYSr5",
     title: "WOLFGARTEN Steel Multi Star Culti-Weeder without Handle for Farms",
-    imgUrl:"https://images-eu.ssl-images-amazon.com/images/I/61f0YRC22oL._AC_UL160_SR160,160_.jpg",
+    imgUrl:
+      "https://images-eu.ssl-images-amazon.com/images/I/61f0YRC22oL._AC_UL160_SR160,160_.jpg",
     price: 1270,
-    price_old:2000,
-    discount:37, 
+    price_old: 2000,
+    discount: 37,
     rating: 3.5,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Farming Tools",
+    productDetails: {
+      brand: "WOLFGARTEN",
+      material: "Steel",
+      Weight: "520 grams",
+    },
+    description: `Made In Germany
+      Optional handle choice- 140cm, 150cm, 170cm (To be Purchased Saperatly of your choice)
+      For hoeing, weeding and trenching
+      Ideal for working around broad leaved shrubs and plants
+      Part of the multi-change click system-any handle fits 50 tool heads`,
   },
   {
-    _id: uuid(),
-    title: "FULLFILLBUY Farmer Safety Shoes, Paddy shoes, Agriculture Safety Shoes.",
-    imgUrl:"https://m.media-amazon.com/images/I/31TYpYBcsoL._AC_UL320_.jpg",
+    _id: "ZVZzMdoL",
+    title:
+      "FULLFILLBUY Farmer Safety Shoes, Paddy shoes, Agriculture Safety Shoes.",
+    imgUrl: "https://m.media-amazon.com/images/I/31TYpYBcsoL._AC_UL320_.jpg",
     price: 1499,
-    price_old:2000,
-    discount:25, 
+    price_old: 2000,
+    discount: 25,
     rating: 2.5,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Safety Wearables",
+    productDetails: {
+      brand: "FULLFILLBUY",
+      material: "PVC material",
+      Weight: "520 grams",
+      dimension: "30 x 10 x 25 cm",
+    },
+    description: `Sole: Polyvinyl Chloride
+      Closure: Pull On
+      Shoe Width: Wide
+      High quality new PVC material, No recycled material
+      Softness, flexibility, durable and unbreakable
+      Seamless forming, military quality, long wear and no leakage
+      Wear-resisting outsole, anti-slip and shock absorption
+      High quality and easy to clean`,
   },
   {
-    _id: uuid(),
-    title: "Pick Ur Needs® Prime Metal 100w Rechargeable Waterproof Led Torch Light.",
-    imgUrl:"https://m.media-amazon.com/images/I/61sXMk18mRL._AC_UL320_.jpg",
+    _id: "2nQRAjxP",
+    title:
+      "Pick Ur Needs® Prime Metal 100w Rechargeable Waterproof Led Torch Light.",
+    imgUrl: "https://m.media-amazon.com/images/I/61sXMk18mRL._AC_UL320_.jpg",
     price: 1799,
-    price_old:2695,
-    discount:33, 
+    price_old: 2695,
+    discount: 33,
     rating: 4.0,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Electronic Devices",
+    productDetails: {
+      brand: "Pick Ur Needs®",
+      voltage: "120 Volts",
+      LuminousFlux: "7000 Lumen",
+      Battery: "Lithium_ion",
+    },
+    description: `Rechargeable LED Flashlight Spotlight Waterproof Handheld Searchlight
+      Easy to hold
+      In your home, outside, in the car… it’s THE flashlight for almost any situation.
+      Thanks to its lightweight and compact size,
+      the Pick Ur Needs Rechargeable Spotlight is designed for a wide variety of heavy-duty uses.
+      Slips easily into backpacks for hunting, fishing, camping, spelunking, hiking, or walking.
+      Can also be stored on a boat, in a camper, or in your automobile`,
   },
   {
-    _id: uuid(),
-    title: "3M Polycarbonate Safety Goggles for Chemical Splash,Pack of 1,Clear",
-    imgUrl:"https://m.media-amazon.com/images/I/51tkdrj3cAL._AC_UL320_.jpg",
+    _id: "VOJG0b5b",
+    title:
+      "3M Polycarbonate Safety Goggles for Chemical Splash,Pack of 1,Clear",
+    imgUrl: "https://m.media-amazon.com/images/I/51tkdrj3cAL._AC_UL320_.jpg",
     price: 110,
-    price_old:115,
-    discount:4, 
+    price_old: 115,
+    discount: 4,
     rating: 2.1,
     isBestSeller: false,
     isOutOfStock: true,
     categoryName: "Safety Wearables",
+    productDetails: {
+      color: "transparent",
+      Weight: "100 grams",
+      Material: "Polycarbonate",
+      BrandName: "3M",
+    },
+    description: `Protects from chemical splashes
+      Polycarbonate frame or lens
+      Wear over prescription lenses, meets ANSI Z87.1-2003
+      4 one-way vents allow ventilation and does not allow splashes to enter
+      colour: clear`,
   },
   {
-    _id: uuid(),
-    title: "Bayer CropScience Confidor Insecticide for Aphids, Whitefly,Control (50ML)",
-    imgUrl:"https://m.media-amazon.com/images/I/41or-q4ie5L._AC_UY218_.jpg",
+    _id: "dF0FQvJL",
+    title:
+      "Bayer CropScience Confidor Insecticide for Aphids, Whitefly,Control (50ML)",
+    imgUrl: "https://m.media-amazon.com/images/I/41or-q4ie5L._AC_UY218_.jpg",
     price: 194,
-    price_old:230,
-    discount:16, 
+    price_old: 230,
+    discount: 16,
     rating: 4.2,
     isBestSeller: false,
     isOutOfStock: true,
     categoryName: "Pesticides",
+    productDetails: {
+      brand: "Bayer CropScience Limited",
+      weight: "120 g",
+      Dimensions: "45 x 5 x 149 Millimeters",
+    },
+    description: `Imidacloprid belongs to chemical class of neonicotinoid insecticides. 
+    It exhibits excellent systemic properties and a significant residual activity.;
+    Imidacloprid has a broad spectrum of activity, particularly against suckinginsects,
+    various species of beetles, some species of flies, leaf miners and termites;
+    Its outstanding biological efficacy, especially its excellent root-systemicproperties,
+    its broad spectrum of activity, good long lasting effect - combinedwith low application rates
+    and good plant compatibility, has made the product the first choice of the farmer;`,
   },
   {
-    _id: uuid(),
-    title: "Katra Fertilizers Lysorus (1 g+50 ml) Anti-Virus and Bacteria Virucide for Virus Control",
-    imgUrl:"https://m.media-amazon.com/images/I/81ZCu2bU-+L._AC_UY218_.jpg",
+    _id: "l07mWMm3",
+    title:
+      "Katra Fertilizers Lysorus (1 g+50 ml) Anti-Virus and Bacteria Virucide for Virus Control",
+    imgUrl: "https://m.media-amazon.com/images/I/81ZCu2bU-+L._AC_UY218_.jpg",
     price: 499,
-    price_old:650,
-    discount:23, 
+    price_old: 650,
+    discount: 23,
     rating: 2.2,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Fertilizers",
+    productDetails: {
+      brand: "Katra Fertilizers",
+      Item_Form: "Powder",
+      Liquid_Volume: "0.05 Litres",
+    },
+    description: `Lysorus (Anti-virus & bacteria) is an multi purpose enzyme which protect plants from bacteria and virus, it is capable of breaking the chemical bonds of the outer cell wall of the bacteria and virus.
+      Lysorus has true activity against bacteria and virus leading to long term protection of crop from bacterial and viral infection.
+      Lysorus is 100% toxic free product. Its uses is environmentally safe.
+      Doses : 1gm lysorus powder + 50 ml activator / acer Method of application :
+      First dissolve 1gm lysorus powder then mix 50 ml Activator into 150 ltr water then spray it on crop of 1 acre.
+      Doses: 1 Pack is Sufficient for 10-time use in 1 Ltr water. Method of Application:-Mix .1gm Powder with 5ml activator(which is already given in the pack) in 1 Ltr. water and spray freely on the plant, Shake well before use.`,
   },
   {
-    _id: uuid(),
-    title: "GREEN WORLD Hybrid Bajra/Pearl Millet Seeds For Farming Or Agriculture (1/2 Kg Seeds)",
-    imgUrl:"https://m.media-amazon.com/images/I/51nvZ2K9eEL._AC_UL320_.jpg",
+    _id: "hKRD5d0S",
+    title:
+      "GREEN WORLD Hybrid Bajra/Pearl Millet Seeds For Farming Or Agriculture (1/2 Kg Seeds)",
+    imgUrl: "https://m.media-amazon.com/images/I/51nvZ2K9eEL._AC_UL320_.jpg",
     price: 199,
-    price_old:299,
-    discount:33, 
+    price_old: 299,
+    discount: 33,
     rating: 3.2,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Seeds",
+    productDetails: {
+      brand: "Green World",
+      Colour: "Green",
+      Item_Weight: "500 Grams",
+      Special_Feature: "Drought Tolerant",
+    },
+    description: `GREEN WORLD Hybrid Bajra/Pearl Millet Seeds For Farming Or Agriculture (1/2 Kg Seeds)`,
   },
   {
-    _id: uuid(),
+    _id: "s3QBAf2P",
     title: "ONLY FOR ORGANIC UV Stabilized Plastic Shade Net, 3x10m (Green)",
-    imgUrl:"https://m.media-amazon.com/images/I/91vmzczsk3L._AC_UL320_.jpg",
+    imgUrl: "https://m.media-amazon.com/images/I/91vmzczsk3L._AC_UL320_.jpg",
     price: 1199,
-    price_old:2100,
-    discount:43, 
+    price_old: 2100,
+    discount: 43,
     rating: 4.2,
     isBestSeller: false,
     isOutOfStock: false,
     categoryName: "Farming Tools",
+    productDetails: {
+      brand: "ONLY FOR ORGANIC",
+      Weight: "420 g",
+      dimensions: "28.5 x 19.4 x 8.3 cm; 420 Grams",
+    },
+    description: `UV Stabilized Agro-Shade Nets - 50% Dimension :Width : 3 Meter Length : As per Your requirement (Upto 50 meter per roll). This nets are U.V. stabilized with higher degree of stabilization to prevent nets from degradation by Ultra violet ray of sun and thus they last longer than others. Â· It helps in controlling temperature. light, water and moisture of nature as per requirement resulting in best output and quality of crop Â· 
+    Green Shade Net protects the crop against the infrared (I.R.) and Ultraviolet Rays (U.V.).
+    Protects the crop against the heavy rain and air Â· 
+    Green house enables the farmer to best utilize the waste land and increase the cultivation area for increasing the output Â· With green net even small piece of land can yield in high output Â· 
+    The level of Carbon dioxide remains high in green house which increases the photosynthesis process and results in higher yield of the crop cultivated Â· With green house the environment of the crop can be controlled resulting in healthy crop with better quality and better yield
+    `,
   },
 ];
