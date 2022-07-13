7ef4a44d2da2edef7176108ad027cdcab051f4d6
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-07-13 18:30:22 +0530

    Create wishListReducer.js

---
+++ src/reducers/wishListReducer.js
@@ -0,0 +1,16 @@
+export const wishListReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "ADD-TO-WISHLIST":
+      return { ...state, wishlistItems: payload };
+    case "REMOVE-FROM-WISHLIST":
+      return { ...state, wishlistItems: payload };
+    case "CLEAR-WISHLIST":
+      return wishListInitialState;
+    default:
+      state;
+  }
+};
+
+export const wishListInitialState = {
+  wishlistItems: [],
+};
