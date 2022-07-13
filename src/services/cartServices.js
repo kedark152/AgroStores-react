01d52b6bb3320bd2904757103c801089586843ad
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-07-13 18:18:18 +0530

    feat: cart api calls for add/remove products

---
+++ src/services/cartServices.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const addItemToCart = async ({ auth, itemDetails, dispatchCart }) => {
+  try {
+    const response = await axios({
+      url: "/api/user/cart",
+      method: "post",
+      headers: {
+        authorization: auth.token,
+      },
+      data: {
+        product: itemDetails,
+      },
+    });
+
+    dispatchCart({
+      type: "ADD-TO-CART",
+      payload: response.data.cart,
+    });
+    toast.success("Item Added to Cart");
+  } catch (error) {
+    console.log("Add Item to Cart Error", error);
+    toast.error("Error to add Item in Cart");
+  }
+};
+
+export const removeItemFromCart = async ({ auth, itemId, dispatchCart }) => {
+  try {
+    const response = await axios.delete(`/api/user/cart/${itemId}`, {
+      headers: { authorization: auth.token },
+    });
+   
+    dispatchCart({
+      type: "REMOVE-FROM-CART",
+      payload: response.data.cart,
+    });
+    toast.success("Item Removed from Cart");
+  } catch (error) {
+    console.log("Remove Item from Cart Error", error);
+    toast.error("Error to remove Item from Cart");
+  }
+};
