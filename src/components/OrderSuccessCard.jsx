bd82d4e492b7fa7d22889c6c0d0714566bdfee37
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-08-07 18:45:59 +0530

    feat: updated order success card with order id

--- src/components/OrderSuccessCard.jsx
+++ src/components/OrderSuccessCard.jsx
@@ -1,6 +1,9 @@
 import "../styles/components/orderCard.css";
 import { Link } from "react-router-dom";
 export const OrderSuccessCard = ({ paymentId }) => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
   return (
     <>
       <div className="order-box flex-column">
@@ -9,9 +12,12 @@ export const OrderSuccessCard = ({ paymentId }) => {
           className="success-tick-img"
         />
         <h2 className="text-center">Your Order Has Been Placed Successfully</h2>
+        <h3 className="text-center payment-text">
+          Order ID: {"OD" + getRandomInt(9999999999)}
+        </h3>
         <h3 className="text-center payment-text">Payment ID: {paymentId}</h3>
         <h4 className="text-center email-msg-text">
-          You will receive order Details soon on your email
+          You will receive order details soon on your email
         </h4>
 
         <Link to="/products" className="btn btn-solid btn-continue-shopping">
