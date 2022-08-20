9c8eade86233cad45e10c2f6786442520b40dca5
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-08-20 14:19:52 +0530

    refactor: filters bar- added cursor pointer & labels clickable

--- src/components/FiltersBar.js
+++ src/components/FiltersBar.js
@@ -77,20 +77,26 @@ export const FiltersBar = () => {
         </div>
         <div className="categories-filter">
           {GetFeaturedCategories().map((item) => (
-            <div key={item._id} className="items align-center">
-              <input
-                type="checkbox"
-                name={item.categoryName}
-                id={item._id}
-                checked={filterState.categoryNames.includes(item.categoryName)}
-                onChange={(e) =>
-                  dispatchFilters({
-                    type: "CATEGORIES",
-                    payload: e.target.name,
-                  })
-                }
-              />
-              <label className="mg-xsm fw-bold" htmlFor={item._id}>
+            <div key={item._id} className="items">
+              <label
+                className="mg-y-xsm fw-bold align-center"
+                htmlFor={item._id}
+              >
+                <input
+                  type="checkbox"
+                  name={item.categoryName}
+                  id={item._id}
+                  checked={filterState.categoryNames.includes(
+                    item.categoryName
+                  )}
+                  onChange={(e) =>
+                    dispatchFilters({
+                      type: "CATEGORIES",
+                      payload: e.target.name,
+                    })
+                  }
+                />
+
                 {item.categoryName}
               </label>
             </div>
@@ -103,17 +109,21 @@ export const FiltersBar = () => {
         </div>
         <div className="ratings-filter">
           {ratingsInput.map((item) => (
-            <div key={item.id} className="items align-center">
-              <input
-                type="radio"
-                name="star-ratings"
-                id={item.star}
-                checked={filterState.ratings == item.star}
-                onChange={(e) =>
-                  dispatchFilters({ type: "RATINGS", payload: e.target.id })
-                }
-              />
-              <label className="mg-xsm fw-bold" htmlFor={item.title}>
+            <div key={item.id} className="items">
+              <label
+                className="mg-y-xsm fw-bold align-center"
+                htmlFor={item.title}
+              >
+                <input
+                  type="radio"
+                  name="star-ratings"
+                  id={item.title}
+                  checked={filterState.ratings == item.star}
+                  onChange={() =>
+                    dispatchFilters({ type: "RATINGS", payload: item.star })
+                  }
+                />
+
                 {item.label}
               </label>
             </div>
@@ -124,50 +134,61 @@ export const FiltersBar = () => {
           <p className="para-md fw-bold">Sort By</p>
         </div>
         <div className="sort-by-filter">
-          <div className="items align-center">
-            <input
-              type="radio"
-              name="sort-radio"
-              id="low-high"
-              checked={filterState.sortBy === "PRICE-LOW-TO-HIGH"}
-              onChange={() =>
-                dispatchFilters({ type: "SORT", payload: "PRICE-LOW-TO-HIGH" })
-              }
-            />
-            <label className="mg-xsm fw-bold" htmlFor="low-high">
+          <div className="items">
+            <label className="mg-y-xsm fw-bold align-center" htmlFor="low-high">
+              <input
+                type="radio"
+                name="sort-radio"
+                id="low-high"
+                checked={filterState.sortBy === "PRICE-LOW-TO-HIGH"}
+                onChange={() =>
+                  dispatchFilters({
+                    type: "SORT",
+                    payload: "PRICE-LOW-TO-HIGH",
+                  })
+                }
+              />
               Low to High
             </label>
           </div>
-          <div className="items align-center">
-            <input
-              type="radio"
-              name="sort-radio"
-              id="high-low"
-              checked={filterState.sortBy === "PRICE-HIGH-TO-LOW"}
-              onChange={() =>
-                dispatchFilters({ type: "SORT", payload: "PRICE-HIGH-TO-LOW" })
-              }
-            />
-            <label className="mg-xsm fw-bold" htmlFor="high-low">
+          <div className="items ">
+            <label className="mg-y-xsm fw-bold align-center" htmlFor="high-low">
+              <input
+                type="radio"
+                name="sort-radio"
+                id="high-low"
+                checked={filterState.sortBy === "PRICE-HIGH-TO-LOW"}
+                onChange={() =>
+                  dispatchFilters({
+                    type: "SORT",
+                    payload: "PRICE-HIGH-TO-LOW",
+                  })
+                }
+              />
               High to Low
             </label>
           </div>
         </div>
         {/* Hide Out of Stock */}
         <div className="filter-title mg-top-sm mg-bottom-xsm">
-          <p className="para-md fw-bold">Out of Stock</p>
+          <p className="para-md fw-bold">Product Availability</p>
         </div>
         <div className="out-of-stock-filter">
-          <div className="items align-center">
-            <input
-              type="checkbox"
-              name="toggle-outstock"
-              id="out-of-stock-toggle"
-              checked={filterState.removeOutOfStock}
-              onChange={() => dispatchFilters({ type: "REMOVE-OUT-OF-STOCK" })}
-            />
-            <label className="mg-xsm fw-bold" htmlFor="out-of-stock-toggle">
-              Hide Out of Stock Products
+          <div className="items ">
+            <label
+              className="mg-y-xsm fw-bold align-center"
+              htmlFor="out-of-stock-toggle"
+            >
+              <input
+                type="checkbox"
+                name="toggle-outstock"
+                id="out-of-stock-toggle"
+                checked={filterState.removeOutOfStock}
+                onChange={() =>
+                  dispatchFilters({ type: "REMOVE-OUT-OF-STOCK" })
+                }
+              />
+              Hide Out of Stock
             </label>
           </div>
         </div>
